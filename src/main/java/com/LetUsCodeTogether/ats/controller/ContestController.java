f07320d71634de4b87dcb05c24f2dd3eef81d752
saigirishpabbathi <saigirishpabbathi.com@gmail.com>
2024-12-16 17:50:37 -0500

    Controller Cross-Origin

--- src/main/java/com/LetUsCodeTogether/ats/controller/ContestController.java
+++ src/main/java/com/LetUsCodeTogether/ats/controller/ContestController.java
@@ -3,13 +3,11 @@ package com.LetUsCodeTogether.ats.controller;
 import com.LetUsCodeTogether.ats.entity.Contest;
 import com.LetUsCodeTogether.ats.service.ContestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/contests")
 public class ContestController {
